5a55b59dea48968ab14c95c290190ba62955e2a5
Devon Deason <devon.d.deason@gmail.com>
2020-04-14 16:41:49 -0600

    env vars

--- src/Firestore/Firestore.js
+++ src/Firestore/Firestore.js
@@ -1,16 +1,17 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCShNITY2hSLvmxaTQRt_avZ-9k0CO1Sw",
-  authDomain: "movie-mogul-f3823.firebaseapp.com",
-  databaseURL: "https://movie-mogul-f3823.firebaseio.com",
-  projectId: "movie-mogul-f3823",
-  storageBucket: "movie-mogul-f3823.appspot.com",
-  messagingSenderId: "693809156342",
-  appId: "1:693809156342:web:fd87d019c4ae0ad7501cd7",
-  measurementId: "G-HVBEBL65PL",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
