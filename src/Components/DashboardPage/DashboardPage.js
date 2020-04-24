dabe1d04294035ea40455946accc656ef2416804
Devon Deason <devon.d.deason@gmail.com>
2020-04-24 07:51:56 -0600

    separate movies and tv

--- src/Components/DashboardPage/DashboardPage.js
+++ src/Components/DashboardPage/DashboardPage.js
@@ -1,21 +1,54 @@
 import React from "react";
+
 import RequestList from "../RequestList/RequestList";
-import { Container, Header } from "semantic-ui-react";
+import {
+  Container,
+  Header,
+  Divider,
+  Grid,
+  Responsive,
+} from "semantic-ui-react";
+import VisibilitySortDropdown from "./../VisibilitySortDropdown/VisibilitySortDropdown";
+import NewRequestBtn from "./../ActionBtns/NewRequestBtn/NewRequestBtn";
 
 const DashboardPage = () => {
   return (
-    <Container>
-      <Header
-        as="h1"
-        textAlign="center"
-        dividing
-        style={{ padding: "2rem" }}
-        inverted
-      >
-        Dashboard
-      </Header>
-      <RequestList />
-    </Container>
+    <>
+      <Container>
+        <Header
+          as="h1"
+          textAlign="center"
+          dividing
+          style={{ padding: "2rem" }}
+          inverted
+        >
+          Dashboard
+        </Header>
+        <Grid padded>
+          <Grid.Row>
+            <Header as="h3" inverted>
+              Pending Requests
+            </Header>
+          </Grid.Row>
+          <VisibilitySortDropdown />
+
+          <Responsive minWidth={992}>
+            <NewRequestBtn />
+          </Responsive>
+        </Grid>
+
+        <Header as="h3" inverted>
+          Movies
+        </Header>
+        <RequestList type="movie" />
+        <Divider hidden />
+
+        <Header as="h3" inverted>
+          TV Shows
+        </Header>
+        <RequestList type="tv" />
+      </Container>
+    </>
   );
 };
 
--- src/Components/RequestList/RequestList.js
+++ src/Components/RequestList/RequestList.js
@@ -1,41 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { Header, Card, Grid, Responsive } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 import RequestItem from "./RequestItem/RequestItem";
 import moment from "moment";
-import VisibilitySortDropdown from "../VisibilitySortDropdown/VisibilitySortDropdown";
-import NewRequestBtn from "../ActionBtns/NewRequestBtn/NewRequestBtn";
 
-const RequestList = ({ requests }) => {
+const RequestList = ({ requests, type }) => {
   return (
     <>
-      <Grid centered style={{ padding: "2rem" }}>
-        <Grid.Row>
-          <Header as="h3" inverted>
-            Pending Requests
-          </Header>
-        </Grid.Row>
-        <VisibilitySortDropdown />
-
-        <Responsive minWidth={992}>
-          <NewRequestBtn />
-        </Responsive>
-      </Grid>
-
-      <Card.Group centered>
+      <Card.Group>
         {requests.map((item) => {
-          return (
-            <RequestItem
-              createdAt={moment(item.createdAt).fromNow()}
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              type={item.type}
-              userName={item.userName}
-              note={item.note}
-            />
-          );
+          console.log(item.type);
+          if (item.type === type) {
+            return (
+              <RequestItem
+                createdAt={moment(item.createdAt).fromNow()}
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                type={item.type}
+                userName={item.userName}
+                note={item.note}
+              />
+            );
+          }
         })}
       </Card.Group>
     </>
